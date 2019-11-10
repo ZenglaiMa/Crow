824e3558ff4a71110fe7d4df37c121ede32dcc9b
马增来 <1303035373@qq.com>
2019-11-10 16:55:40 +0800

    init server

---
+++ crow-server/src/com/happier/crow/config/AppConfig.java
@@ -0,0 +1,46 @@
+package com.happier.crow.config;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+import com.jfinal.template.Engine;
+
+public class AppConfig extends JFinalConfig {
+
+	@Override
+	public void configConstant(Constants me) {
+		me.setDevMode(true);
+	}
+
+	@Override
+	public void configRoute(Routes me) {
+
+	}
+
+	@Override
+	public void configEngine(Engine me) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void configPlugin(Plugins me) {
+
+	}
+
+	@Override
+	public void configInterceptor(Interceptors me) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void configHandler(Handlers me) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
