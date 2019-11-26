7d76e4ec5b3dc83208cb78654dbed304503721f1
zhaoyuhanlei <1776670476@qq.com>
2019-11-26 14:40:19 +0800

    add alarm UI

---
+++ crow/app/src/main/java/com/happier/crow/children/ChildrenSetAlarmActivity.java
@@ -0,0 +1,15 @@
+package com.happier.crow.children;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.happier.crow.R;
+
+public class ChildrenSetAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_children_set_alarm);
+    }
+}
