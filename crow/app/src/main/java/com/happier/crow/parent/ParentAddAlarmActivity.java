60410eba21f582226989e5abc05681785e09e1a6
马增来 <1303035373@qq.com>
2019-12-05 17:38:46 +0800

    change some layout

--- crow/app/src/main/java/com/happier/crow/parent/ParentAddAlarmActivity.java
+++ crow/app/src/main/java/com/happier/crow/parent/ParentAddAlarmActivity.java
@@ -4,17 +4,16 @@ import android.content.Intent;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.happier.crow.R;
-import com.happier.crow.children.ChildrenSetAlarmActivity;
 import com.happier.crow.constant.Constant;
 import com.happier.crow.entities.Alarm;
 
@@ -32,6 +31,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class ParentAddAlarmActivity extends AppCompatActivity {
+
     private EditText etType = null;
     private EditText etDescription = null;
     private Spinner spMOra = null;
@@ -44,6 +44,7 @@ public class ParentAddAlarmActivity extends AppCompatActivity {
     private String hour = "";
     private String minute = "";
     private static final String PARENT_SET_INFO_PATH = "/alarm/setAlarmParent";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +53,7 @@ public class ParentAddAlarmActivity extends AppCompatActivity {
         initData();
         linsteners();
     }
+
     private void linsteners() {
         spMOra.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -63,6 +65,7 @@ public class ParentAddAlarmActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+
         spHour.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -73,6 +76,7 @@ public class ParentAddAlarmActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+
         spMinute.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -83,6 +87,7 @@ public class ParentAddAlarmActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+
         btnAlarmSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,6 +101,7 @@ public class ParentAddAlarmActivity extends AppCompatActivity {
             }
         });
     }
+
     private void setOrModify() {
         OkHttpClient client = new OkHttpClient();
         int pid = getSharedPreferences("authid", MODE_PRIVATE).getInt("pid", 0);
@@ -121,11 +127,11 @@ public class ParentAddAlarmActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 String result = response.body().string();
-                if (result.equals("0")){
+                if (result.equals("0")) {
                     Looper.prepare();
                     Toast.makeText(ParentAddAlarmActivity.this, "提醒设置失败", Toast.LENGTH_LONG).show();
                     Looper.loop();
-                }else {
+                } else {
                     Looper.prepare();
                     Toast.makeText(ParentAddAlarmActivity.this, "提醒设置成功", Toast.LENGTH_LONG).show();
                     EventBus.getDefault().post(result);
@@ -134,6 +140,7 @@ public class ParentAddAlarmActivity extends AppCompatActivity {
             }
         });
     }
+
     private void initData() {
         etType = findViewById(R.id.z_et_alarm_type_parent);
         etDescription = findViewById(R.id.z_et_alarm_description_parent);
@@ -142,6 +149,7 @@ public class ParentAddAlarmActivity extends AppCompatActivity {
         spMinute = findViewById(R.id.z_spinner_minute_parent);
         btnAlarmSubmit = findViewById(R.id.z_btn_alarm_submit_parent);
     }
+
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void handleResult(String result) {
         Alarm alarm = new Alarm();
@@ -153,5 +161,15 @@ public class ParentAddAlarmActivity extends AppCompatActivity {
         intent.putExtra("newData", new Gson().toJson(alarm));
         setResult(99, intent);
         finish();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+            overridePendingTransition(R.anim.left_in, R.anim.right_out);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
--- crow/app/src/main/java/com/happier/crow/parent/ParentAlarmActivity.java
+++ crow/app/src/main/java/com/happier/crow/parent/ParentAlarmActivity.java
@@ -5,17 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.happier.crow.R;
-import com.happier.crow.children.ChildrenIndexActivity;
 import com.happier.crow.constant.Constant;
 import com.happier.crow.entities.Alarm;
 
@@ -27,10 +23,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -40,13 +34,12 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class ParentAlarmActivity extends AppCompatActivity {
-    private ImageView ivParentAlertBack = null;
-    private TextView tvParentAlertBack = null;
     private ListView lvParentShowAlarm = null;
     private LinearLayout llParentAddAlarm = null;
     public List<Alarm> list = new ArrayList<>();
     private ParentAlarmAdapter adapter;
     private static final String PARENT_SET_INFO_PATH = "/alarm/getAlarmParent";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,37 +51,25 @@ public class ParentAlarmActivity extends AppCompatActivity {
     }
 
     private void linsteners() {
-        ivParentAlertBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-        tvParentAlertBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
         llParentAddAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ParentAlarmActivity.this, ParentAddAlarmActivity.class);
                 startActivityForResult(intent, 100);
+                overridePendingTransition(R.anim.right_in, R.anim.left_out);
             }
         });
 
     }
 
-    private void initData(){
-        ivParentAlertBack = findViewById(R.id.z_iv_parent_alert_back);
-        tvParentAlertBack = findViewById(R.id.z_tv_parent_alert_back);
+    private void initData() {
         lvParentShowAlarm = findViewById(R.id.z_lv_parent_show_alarm);
         llParentAddAlarm = findViewById(R.id.z_ll_parent_add_alarm);
         setOrModify();
         adapter = new ParentAlarmAdapter(list, this, R.layout.parent_alarm_item);
         lvParentShowAlarm.setAdapter(adapter);
     }
+
     private void setOrModify() {
         OkHttpClient client = new OkHttpClient();
         int pid = getSharedPreferences("authid", MODE_PRIVATE).getInt("pid", 0);
@@ -113,7 +94,7 @@ public class ParentAlarmActivity extends AppCompatActivity {
                 JSONArray ja = null;
                 try {
                     ja = new JSONArray(result);
-                    for (int i=0; i<ja.length(); i++){
+                    for (int i = 0; i < ja.length(); i++) {
                         JSONObject jsTemp = ja.getJSONObject(i);
                         String strTemp = jsTemp.getString("attrs");
                         Alarm alarm = new Gson().fromJson(strTemp, Alarm.class);
@@ -126,17 +107,29 @@ public class ParentAlarmActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == 100 && resultCode == 99){
+        if (requestCode == 100 && resultCode == 99) {
             Alarm alarm = new Gson().fromJson(data.getStringExtra("newData"), Alarm.class);
             list.add(alarm);
             adapter.notifyDataSetChanged();
         }
     }
+
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void handleLoginResult(List<Alarm> listTemp) {
-        this.list.add(listTemp.get(listTemp.size()-1));
+        this.list.add(listTemp.get(listTemp.size() - 1));
         adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+            overridePendingTransition(R.anim.left_in, R.anim.right_out);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
